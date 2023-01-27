ef17df01175e6bc25c8f7875df6feac3df1d4ea5
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2023-01-26 16:36:04 -0800

    Update rf_risklist_api_curl_redirect_to_file.sh

--- scripts/rf_risklist_api_curl_redirect_to_file.sh
+++ scripts/rf_risklist_api_curl_redirect_to_file.sh
@@ -5,9 +5,9 @@
 CURL=$(which curl)
 CURLARGS="-H \"X-RFToken: $RFAPITOKEN\" -f"
 # OR 
-# CURLARGS="-H "X-RFToken: $RFAPITOKEN" -f -o iprisklist_$(date +"%Y_%m_%d_%H_%M_%S_%s").csv""
+# CURLARGS="-H "X-RFToken: $RFAPITOKEN" -f -v -o iprisklist_$(date +"%Y_%m_%d_%H_%M_%S_%s").csv""
 # Enable the below statement by removing the comment and sniff the traffic for troubleshooting:
-# CURLARGS="-H "X-RFToken: $RFAPITOKEN" -f -p 8080""
+# CURLARGS="-H "X-RFToken: $RFAPITOKEN" -f -v -p 8080""
 RFIPRISKLIST="https://api.recordedfuture.com/v2/ip/risklist?format=csv%2Fsplunk&gzip=false"
 # Ensure to export the API Token env variable into `~/.bashrc` for the curl command body to function
 # $ echo "export RFAPITOKEN=X" >> ~/.bashrc
@@ -20,6 +20,6 @@ RFIPRISKLIST="https://api.recordedfuture.com/v2/ip/risklist?format=csv%2Fsplunk&
 # raw="$($CURL $CURLARGS $RFIPRISKLIST)"
 
 # or you can redirect it into a file:
-#$CURL $CURLARGS $RFIPRISKLIST 
+#$CURL $CURLARGS $RFIPRISKLIST -f -v 
 # When invoking the script in this method, you do not need the curl `-o` argument 
-#$CURL $CURLARGS $RFIPRISKLIST >> ./unparsed_iprisklist_$(date +"%Y_%m_%d_%H_%M_%S_%s").csv
+#$CURL $CURLARGS $RFIPRISKLIST -f -v >> ./unparsed_iprisklist_$(date +"%Y_%m_%d_%H_%M_%S_%s").csv
