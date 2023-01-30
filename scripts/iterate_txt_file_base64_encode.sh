6c7dde229905a53bb0c7ffc4c4c496af12ee6049
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2023-01-30 13:49:51 -0800

    Create iterate_txt_file_base64_encode.sh

---
+++ scripts/iterate_txt_file_base64_encode.sh
@@ -0,0 +1,12 @@
+# which bash
+#!/usr/bin/bash
+
+# This script reads a static filename's contents set by a Python script ran by a crontab rule and pipes the output to a base64 encoded form on a line-separated basis, suitable as a Suricata dataset.
+
+for line in `cat /development/rf-ip-risklists/parsed-ip-risklists/iprisklist_rf_ip_risklist_ips_latest.txt`;
+do
+	echo $line | base64
+done > /etc/suricata/rules/domains_base64_rf_base64.lst
+
+# Invoke using the following example:
+# chmod +x <script.sh>
